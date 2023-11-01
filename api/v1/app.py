a480564e32e7e4e3ffb2dafaed8dc6bda39509ae
sindi702 <hamatajsindi@gmail.com>
2023-11-01 11:12:31 +0100

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,16 @@
-#!/usr/bin/python3
-"""Start of Flask app"""
 from flask import Flask
-from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask import jsonify
-from flask_cors import CORS
+from models import storage
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
 @app.teardown_appcontext
-def close_storage(exception):
+def teardown(exception):
+    """Teardown method that calls storage.close()"""
     storage.close()
 
-
-@app.errorhandler(404)
-def handle_404(e):
-    return jsonify(error='Not found'), 404
-
-
-if __name__ == '__main__':
-    """Starting flask server"""
-    host = '0.0.0.0'
-    port = '5000'
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
+if __name__ == "__main__":
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    app.register_blueprint(app_views, url_prefix="/api/v1")
     app.run(host=host, port=port, threaded=True)
